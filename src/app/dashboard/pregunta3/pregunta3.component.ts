e7b1ee1818cd846cf40e21809cd98881295f8d5a
Rod007kn2000 L <i202021234@cibertec.edu.pe>
2024-06-22 21:40:55 -0500

    2do comit preg 4

--- src/app/dashboard/pregunta3/pregunta3.component.ts
+++ src/app/dashboard/pregunta3/pregunta3.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularmaterialModule } from '../../angularmaterial/angularmaterial.module';
 
 @Component({
   selector: 'app-pregunta3',
   standalone: true,
-  imports: [],
+  imports: [AngularmaterialModule, FormsModule],
   templateUrl: './pregunta3.component.html',
   styleUrl: './pregunta3.component.css'
 })
 export class Pregunta3Component {
 
+  cantidad!: number
+  precio!: number
+  total!: number
+  totalConDescuento!: number
+
+  calcularTotal(): void {
+    const valorTotal = this.cantidad * this.precio;
+    if (valorTotal > 200) {
+      this.totalConDescuento = valorTotal * 0.8;
+    } else {
+      this.totalConDescuento = valorTotal;
+    }
+    this.total = valorTotal;
+  }
 }
