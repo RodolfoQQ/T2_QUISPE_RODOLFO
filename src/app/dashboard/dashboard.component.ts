0c1db028cb8f3cfce7e4220902b21233d40c3a7f
Rod007kn2000 L <i202021234@cibertec.edu.pe>
2024-06-22 20:55:11 -0500

    pregunta completada

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { AngularmaterialModule } from '../angularmaterial/angularmaterial.module';
+import { ServiceService } from '../auth/service.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [AngularmaterialModule, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private authService: ServiceService
+  ){
+
+  }
+
+  pregunta1(){
+    this.router.navigate(["pregunta1"], {relativeTo: this.route})
+  }
+
+
+  pregunta2(){
+    this.router.navigate(["pregunta2"], {relativeTo: this.route})
+  }
+
+  pregunta3(){
+    this.router.navigate(["pregunta3"], {relativeTo: this.route})
+  }
+
+  salir(){
+
+  }
 }
+
