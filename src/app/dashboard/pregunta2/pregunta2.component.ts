5af75b29a02cc0085981e5fffa858640c1070b2e
Rod007kn2000 L <i202021234@cibertec.edu.pe>
2024-06-22 21:22:11 -0500

    2do comit pregunta 3

--- src/app/dashboard/pregunta2/pregunta2.component.ts
+++ src/app/dashboard/pregunta2/pregunta2.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularmaterialModule } from '../../angularmaterial/angularmaterial.module';
 
 @Component({
   selector: 'app-pregunta2',
   standalone: true,
-  imports: [],
+  imports: [AngularmaterialModule, FormsModule],
   templateUrl: './pregunta2.component.html',
   styleUrl: './pregunta2.component.css'
 })
 export class Pregunta2Component {
 
+  cantidad: number = 0;
+  total: number | null = null;
+
+  calcularTotal(): void {
+    const precioLlanta = this.cantidad < 5 ? 800 : 700;
+    this.total = this.cantidad * precioLlanta;
+  }
 }
