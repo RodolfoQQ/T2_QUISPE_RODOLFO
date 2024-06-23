cd69660c3a13a4e46c8c298eca4b7cb9a5985ef0
Rod007kn2000 L <i202021234@cibertec.edu.pe>
2024-06-22 21:11:04 -0500

    2.2

--- src/app/dashboard/pregunta1/pregunta1.component.ts
+++ src/app/dashboard/pregunta1/pregunta1.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularmaterialModule } from '../../angularmaterial/angularmaterial.module';
 
 @Component({
   selector: 'app-pregunta1',
   standalone: true,
-  imports: [],
+  imports: [AngularmaterialModule, FormsModule],
   templateUrl: './pregunta1.component.html',
-  styleUrl: './pregunta1.component.css'
+  styleUrls: ['./pregunta1.component.css']
 })
 export class Pregunta1Component {
+  precioUnitario: number = 0;
+  cantidad: number = 0;
+  total: number | null = null;
 
+  calcularTotal(): void {
+    let descuento = 0;
+
+    if (this.cantidad > 20) {
+      descuento = 0.10;
+    } else if (this.cantidad > 10) {
+      descuento = 0.05;
+    }
+
+    const precioTotal = this.precioUnitario * this.cantidad;
+    this.total = precioTotal - (precioTotal * descuento);
+  }
 }
