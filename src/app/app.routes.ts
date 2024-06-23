6b8353cc9765a7e0aa64239fe83f6a2a44b4b9a5
Rod007kn2000 L <i202021234@cibertec.edu.pe>
2024-06-22 21:45:25 -0500

    añadiendo sesionstorage guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Pregunta1Component } from './dashboard/pregunta1/pregunta1.component';
 import { Pregunta2Component } from './dashboard/pregunta2/pregunta2.component';
 import { Pregunta3Component } from './dashboard/pregunta3/pregunta3.component';
+import { guardGuard } from './guard.guard';
 import { PagenotfountComponent } from './pagenotfount/pagenotfount.component';
 
 export const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "dashboard", component: DashboardComponent,
+    canActivate: [guardGuard],
     children: [
       {path: "pregunta1", component: Pregunta1Component},
       {path: "pregunta2", component: Pregunta2Component},
